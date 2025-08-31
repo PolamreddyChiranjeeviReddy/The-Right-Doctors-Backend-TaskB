c0244d32fde311d437b33c99a9987d8db0b57ea2
PolamreddyChiranjeeviReddy <cjr45555@gmail.com>
2025-08-31 10:25:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/person', personRoutes);
-
+app.get('/health', (req, res) => {
+  res.status(200).send("OK");
+});
 app.get('/', (req, res) => {
   res.redirect('/person');
 });
